d980ad4bf0cd6a138a83155b66947ea2d3bcc653
Murielle Dunand <mdunand@mit.edu>
2021-04-29 17:10:14 -0500

    first dec draft (is running)

--- encrypt.py
+++ encrypt.py
@@ -267,15 +267,153 @@ def enc_channel(img, pkb, verbose=False):
     # Output ciphertexts, r, and encrypted image
     return (c, r, X_rk)
 
-def dec(img, ciphertexts, r, pkb, skb):
-    # TODO: implement; should be inverse of enc
-    pass
+def dec(img, ciphertexts, r, pkb, skb, verbose=False):
+    """ Calls dec_channel on each channel in the img. """
+    if verbose:
+        print("Starting encryption...")
+
+    image_dims = img.shape
+    # If image has one channel (grayscale)
+    if len(image_dims)==2:
+        return dec_channel(img, ciphertexts, r, pkb, skb)
+
+    # Else if image has 3 or 4 channels (RGB or RGBA) 
+    if len(image_dims)==3 and (image_dims[2]==3 or image_dims[2]==4): 
+        dec_img = []
+        for channel in range(image_dims[2]):
+            dec_img_channel = dec_channel(img[:,:,channel], ciphertexts[channel], r[channel], pkb, skb, verbose=verbose)
+            dec_img.append(dec_img_channel.T) # (rows, cols) => (cols, rows)
+        dec_img = np.array(dec_img).T # (channels, cols, rows) => (rows, cols, channels)
+        return dec_img
+
+    # Else invalid image
+    return (None, None, None)
+
+def dec_channel(img, ciphertexts, r, pkb, skb, verbose=False):
+    if verbose:
+        print("Encrypting new channel...")
+    # Calculate r
+    M, N = img.shape
+    if verbose:
+        print("Calculated r...")
+    mod_n = (M+1)*(N+1) if (M+1)*(N+1)>256 else (M+1)*(N+1)*256
+
+    # Calculate m, c
+    c = ciphertexts
+    #print(c, len(c), type(c))
+    #print(c[0], type(c[0]))
+    m = [rsa.decrypt(i, skb) for i in c]
+    m_int = [int.from_bytes(m_i, sys.byteorder) for m_i in m]
+    c_int = [int.from_bytes(c_i, sys.byteorder) for c_i in c]
+    if verbose:
+        print("Calculated m, c...")
+
+    # Calculate xyz_500
+    xyz = [mod(1/(abs(m_int[i] - c_int[i]) + r), mod_n) for i in range(3)]
+    for i in range(500):
+        xyz = update_xyz(xyz, mod_n, r)
+
+    # Encryption round variables
+    # X_rk holds the value of the image at start of round rk; X_0 = plain image
+    X_rk = np.array(img, dtype=np.float32) 
+
+    # C_rk holds the value of the encrypted image at start of round rk; C_0 = original encrypted image
+    C_rk = np.array(img, dtype=np.float32) 
+    # xyz_prev holds the value xyz_{500 + rk(MN)} (xyz_500 before rk loop)
+    xyz_prev = xyz 
+
+    # Encryption round
+    for rk in range(5):
+        if verbose:
+            print("In encryption round {}...".format(rk))
+        # xyzs = [xyz_{500+rk(MN)}, xyz_{500+rk(MN)+1}, ..., xyz_{500+rk(MN)+MN}
+        xyzs = [list(xyz_prev)] 
+        for i in range(M*N):
+            xyzs.append(update_xyz(xyzs[-1], mod_n, r))
+        xyz_prev = xyzs[-1] # update xyz_prev to last xyzs array
+
+        # Calculate x_k', y_k', z_k', w_k', and s_k
+        xk_primes = [] # xk_primes = [x_1', x_2', ..., x_M']
+        for k in range(M):
+            xk_primes.append((np.floor(xyzs[k+1][0] * 1e14)) % (N+1))
+        yk_primes = [] # yk_primes = [y_1', y_2', ..., y_N']
+        for k in range(N):
+            yk_primes.append((np.floor(xyzs[k+1][1] * 1e14)) % (M+1))
+        zk_primes = [] # zk_primes = [z_1', z_2', ..., z_M']
+        for k in range(M):
+            zk_primes.append((np.floor((xyzs[k+1][2] * 0.6 + xyzs[k+1][0] * 0.4) * 1e14)) % (N+1))
+        wk_primes = [] # wk_primes = [w_1', w_2', ..., w_M']
+        for k in range(N):
+            wk_primes.append((np.floor((xyzs[k+1][2] * 0.6 + xyzs[k+1][1] * 0.4) * 1e14)) % (M+1))
+        sk = [] # sk = [s_1, s_2, ..., s_{MN}]
+        for k in range(M*N):
+            sk.append(np.fix(sum(xyzs[k+1]) * 1e14) % 256)
+
+        if verbose:
+            print("Starting row and column permutations...")
+        # Row permutations
+        Xrk_prime = np.zeros((M,N))
+        for i in range(M): # rows
+            for j in range(N): # cols
+                Xrk_prime[i][j] = X_rk[i][int((j+xk_primes[i]-1) % N)]
+
+        # Column permutations
+        Xrk_dp = np.zeros((M,N))
+        for i in range(M):
+            for j in range(N):
+                Xrk_dp[i][j] = Xrk_prime[int((i+yk_primes[i]-1) % M)][j]
+
+        # Discrete cosine transform coefficient matrix
+        F = np.zeros((M,N))
+        for u in range(M):
+            for v in range(N):
+                for i in range(M):
+                    for j in range(N):
+                        F[u][v] += Xrk_dp[i][j] * np.cos(((2*i+1)*np.pi*u)/(2*M)) * np.cos(((2*j+1)*np.pi*v)/(2*N))
+                F[u][v] *= sigma(u,M) * sigma(v,N)
+
+        # Row permutations
+        F_prime = np.zeros((M,N))
+        for i in range(M):
+            for j in range(N):
+                F_prime[i][j] = F[i][int((j+zk_primes[i]-1) % N)]
+
+        # Column permutations
+        F_dp = np.zeros((M,N))
+        for i in range(M):
+            for j in range(N):
+                F_dp[i][j] = F_prime[int((i+wk_primes[i]-1) % M)][j]
+
+        if verbose:
+            print("Starting inverse discrete cosine transform coefficient matrix...")
+        # Inverse discrete cosine transform coefficient matrix
+        G = np.zeros((M,N), dtype=np.float32)
+        for i in range(M):
+            for j in range(N):
+                for u in range(M):
+                    for v in range(N):
+                        G[i][j] += sigma(u,M) * sigma(v,N) * F_dp[i][j] * np.cos(((2*i+1)*np.pi*u)/(2*M)) * np.cos(((2*j+1)*np.pi*v)/(2*N))
+
+        if verbose:
+            print("Generating encrypted image for round {}...".format(rk))
+        # Generate encrypted image for round rk
+        xor_values = xor(xor(G.flatten(), np.array(sk, dtype=np.float32)), C_rk.flatten())
+        for ind in range(M*N):
+            val = xor_values[ind]
+            i = int(ind // N)
+            j = int(ind % N)
+            C_rk[i][j] = val
+
+    # Output decrypted image
+    return C_rk
+
 
 if __name__ == "__main__":
     print("Starting...")
     start = time.time()
     pk, sk = read_keys("rsa-keys/public.pem", "rsa-keys/private.pem")
-    c, r, enc_img = enc(cv2.imread("images/testimage1_32x24.jpg"), pk, verbose=True)
-    print(enc_img)
+    c, r, enc_img = enc(cv2.imread("images/testimage1_32x24.jpg"), pk, verbose=False)
+    dec_img = dec(enc_img, c, r, pk, sk, verbose=True)
+    print(dec_img)
     end = time.time()
     print("Time:", end - start)
