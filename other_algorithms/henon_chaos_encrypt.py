2ee2aa338970c183059e19e1af2528ebdda637ec
Ray Dedhia <22286026+ray-dedhia@users.noreply.github.com>
2021-05-18 09:44:00 -0400

    added parameter to change secret key

--- other_algorithms/henon_chaos_encrypt.py
+++ other_algorithms/henon_chaos_encrypt.py
@@ -8,16 +8,19 @@ def dec(bitSequence):
         decimal = decimal * 2 + int(bit)
     return decimal
 
-#Takes size of the image as an input
-def genTransformationMatrix(m):
-    #Replace Hardcoded Pixel Values
-    #Serves as the initial Parameter and also the symmetric secret key
-    x = 0.1
-    y = 0.1
-    sequenceSize = m * m * 8 #Total Number of bitSequence produced
-    bitSequence = []    #Each bitSequence contains 8 bits
-    byteArray = []      #Each byteArray contains m( i.e 512 in this case) bitSequence
-    TImageMatrix = []   #Each TImageMatrix contains m*n byteArray( i.e 512 byteArray in this case)
+def genTransformationMatrix(m, key=(0.1, 0.1)):
+    """
+    Generates Henon map of size (m x m) using the input symmetric secret key, which is a tuple of floats.
+    """
+    
+    # Variables
+    x, y = key # Get symmetric secret key
+    sequenceSize = m * m * 8 # Total Number of bitSequence produced
+    bitSequence = []    # Each bitSequence contains 8 bits
+    byteArray = []      # Each byteArray contains m bitSequence
+    TImageMatrix = []   # Each TImageMatrix contains m*m byteArra
+
+    # Generate Henon map
     for i in range(sequenceSize):
         #Henon Map formula
         xN = y + 1 - 1.4 * x**2
@@ -32,15 +35,18 @@ def genTransformationMatrix(m):
             bit = 0
         else:
             bit = 1
-        #bits are inserted into bitSequence
+
+        # Bits are inserted into bitSequence
         bitSequence.append(bit)
-        #Each bitSequence is converted into a decimal number
-        #This decimal number is inserted into byteArray
+
+        # Each bitSequence is converted into a decimal number
+        # This decimal number is inserted into byteArray
         if i % 8 == 7:
             decimal = dec(bitSequence)
             byteArray.append(decimal)
             bitSequence = []
-        #ByteArray is inserted into TImageMatrix
+
+        # ByteArray is inserted into TImageMatrix
         byteArraySize = m*8
         if i % byteArraySize == byteArraySize-1:
             TImageMatrix.append(byteArray)
@@ -48,8 +54,11 @@ def genTransformationMatrix(m):
             
     return TImageMatrix
 
-def decryptHenonImage(imageMatrix):
-    transformationMatrix = genTransformationMatrix(len(imageMatrix))
+def decryptHenonImage(imageMatrix, key=(0.1, 0.1)):
+    """
+    Decrypt Henon image. Takes in image array and (optional) secret key as parameters. Default secret key is (0.1, 0.1).
+    """
+    transformationMatrix = genTransformationMatrix(len(imageMatrix), key=key)
 
     henonDecryptedImage = []
     for i in range(len(imageMatrix)):
@@ -60,8 +69,8 @@ def decryptHenonImage(imageMatrix):
 
     return henonDecryptedImage
 
-def pixelManipulation(size, imageMatrix):
-    transformationMatrix = genTransformationMatrix(size)
+def pixelManipulation(size, imageMatrix, key=(0.1, 0.1)):
+    transformationMatrix = genTransformationMatrix(size, key=key)
 
     #Performing Ex-Or Operation between the transformation Matrix and ImageMatrix
     #Storing the result in resultantMatrix
@@ -153,9 +162,11 @@ def array_to_image(imageMatrix):
     # else invalid
     return None
 
-def encrypt(im):
+def encrypt(im, key=(0.1, 0.1)):
     """
     Takes in a PIL image and returns an encrypted Henon Transformed Image as a PIL Image of shape (size, size).
+
+    Takes a PIL Image and an (optional) symmetric secret key as parameters. Default secret key is (0.1, 0.1).
     """
     # get image and shape
     pix = im.load()
@@ -171,21 +182,23 @@ def encrypt(im):
         # individually encrypt each channel
         for channel in range(3):
             channel_matrix = convert_to_array(im, channel)
-            encr_channels.append(pixelManipulation(size, channel_matrix))
+            encr_channels.append(pixelManipulation(size, channel_matrix, key=key))
         encr_img = combine_channels(encr_channels)
         return array_to_image(encr_img)
 
     # if 2D grayscale image
     if len(image_shape) == 2:
-        encr_img = pixelManipulation(size, convert_to_array(im))
+        encr_img = pixelManipulation(size, convert_to_array(im), key=key)
         return array_to_image(encr_img)
     
     # else invalid image
     return None
 
-def decrypt(im):
+def decrypt(im, key=(0.1, 0.1)):
     """
     Takes in encrypted Henon Transformed Image as PIL Image and decrypts it, then returns the decrypted image as a PIL Image.
+
+    Takes a PIL Image and an (optional) symmetric secret key as parameters. Default secret key is (0.1, 0.1).
     """
     # get image and shape
     pix = im.load()
@@ -201,13 +214,13 @@ def decrypt(im):
         # individually decrypt each channel
         for channel in range(3):
             channel_matrix = convert_to_array(im, channel)
-            decr_channels.append(decryptHenonImage(channel_matrix))
+            decr_channels.append(decryptHenonImage(channel_matrix, key=key))
         decr_img = combine_channels(decr_channels)
         return array_to_image(decr_img)
 
     # if 2D grayscale image
     if len(image_shape) == 2:
-        decr_img = decryptHenonImage(convert_to_array(encr_img))
+        decr_img = decryptHenonImage(convert_to_array(encr_img, key=key))
         return array_to_image(decr_img)
     
     # else invalid image
